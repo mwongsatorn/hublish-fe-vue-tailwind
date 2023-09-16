88b41ecacf37045e63952d6a592bf6b7dc09bf5a
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-09-16 13:07:29 +0700

    style: format and lint

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
@@ -7,12 +7,8 @@ import { RouterLink } from 'vue-router';
     <div class="max-w-7xl mx-auto flex items-center h-[56px] px-4">
       <span class="font-bold text-xl uppercase">Hublish</span>
       <div class="ml-auto flex items-center gap-x-4">
-        <RouterLink class="hover:text-green-500" to="/login">
-          Log in
-        </RouterLink>
-        <RouterLink class="hover:text-green-500" to="/signup">
-          Sign up
-        </RouterLink>
+        <RouterLink class="hover:text-green-500" to="/login"> Log in </RouterLink>
+        <RouterLink class="hover:text-green-500" to="/signup"> Sign up </RouterLink>
       </div>
     </div>
   </header>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,8 +2,8 @@
 
 <template>
   <main class="bg-green-500">
-    <section class="max-w-7xl mx-auto px-4 py-36  text-white space-y-4">
-      <div class="font-bold text-5xl uppercase">Lorem ipsum dolor sit amet consectetur </div>
+    <section class="max-w-7xl mx-auto px-4 py-36 text-white space-y-4">
+      <div class="font-bold text-5xl uppercase">Lorem ipsum dolor sit amet consectetur</div>
       <div class="w-1/2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum totam minima quod maiores
         eos saepe adipisci molestiae tenetur maxime nulla!
