3736ff6401c324a0943528e7dd429e267b15922d
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-09-16 12:40:00 +0700

    feat: login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,18 @@ import HomePage from '@/views/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-    path: "/",
-    name: "Home",
-    component: HomePage
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/LoginPage.vue')
+    }
+  ]
 })
 
 export default router
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const username = ref('')
+const password = ref('')
+
+function loginSubmit() {
+  alert(username.value + ' has logged in')
+  username.value = ''
+  password.value = ''
+}
+</script>
+
+<template>
+  <main>
+    <section
+      class="max-w-7xl mx-auto w-full px-4 h-[calc(100vh-56px)] flex items-center justify-center"
+    >
+      <div class="max-w-md w-full py-12 px-6 shadow-lg border">
+        <h1 class="font-bold text-xl">Log in</h1>
+        <form class="" @submit.prevent="loginSubmit()">
+          <div class="mt-4">
+            <label for="username">Username</label>
+            <input
+              v-model="username"
+              id="username"
+              class="block w-full mt-2 border-2 px-4 py-1.5"
+              placeholder="Username"
+              type="text"
+            />
+          </div>
+          <div class="mt-4">
+            <label for="password">Password</label>
+            <input
+              v-model="password"
+              id="password"
+              class="block w-full mt-2 border-2 px-4 py-2"
+              placeholder="Password"
+              type="password"
+            />
+          </div>
+          <button class="ml-auto rounded-lg mt-8 block bg-green-500 px-4 py-2 text-white">
+            Log in
+          </button>
+        </form>
+      </div>
+    </section>
+  </main>
+</template>
