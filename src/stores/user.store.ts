da96f8875636f9331097d77363d8cd1faca73804
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-09-29 21:08:12 +0700

    fix: states in user store

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -3,22 +3,34 @@ import axios from 'axios'
 import { LogInResponseSchema } from '@/schemas/user'
 
 interface UserState {
-  accessToken: string | null
+  user: {
+    email: string
+    username: string
+    name: string
+    bio: string
+    accessToken: string
+  } | null
+  isLoggedIn: boolean
 }
 
 export const useUserStore = defineStore('user', {
   state: (): UserState => ({
-    accessToken: null
+    isLoggedIn: false,
+    user: null
   }),
   actions: {
     async login(details: { username: string; password: string }) {
       const response = await axios.post('/api/users/login', details)
       const result = LogInResponseSchema.safeParse(response.data)
+
       if (!result.success) {
         return { status: false, error: 'Something went wrong please try again later' }
       }
       const { data } = result
-      if (data.status) this.accessToken = data.accessToken
+      if (data.status) {
+        this.user = data.user
+        this.isLoggedIn = true
+      }
       return data
     }
   }
