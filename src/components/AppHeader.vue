25dfc3689fcbd6135e20d4d33533437ce5547574
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-09-29 21:29:33 +0700

    feat: show user details when logged in on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,64 @@
 <script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 import { RouterLink } from 'vue-router'
+import { useUserStore } from '@/stores/user.store'
+import IconChevronDown from '@/components/icons/ChevronDown.vue'
+
+const user = useUserStore()
+
+const isMenuOpened = ref(false)
+const menu = ref<HTMLElement | null>(null)
+
+function handleClickOutside(e: MouseEvent) {
+  console.log(isMenuOpened.value)
+  if (e.target instanceof HTMLElement && !menu.value?.contains(e.target)) isMenuOpened.value = false
+}
+
+onMounted(() => {
+  document.addEventListener('click', handleClickOutside)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClickOutside)
+})
 </script>
 
 <template>
   <header class="shadow-lg">
     <div class="max-w-7xl mx-auto flex items-center h-[56px] px-4">
-      <span class="font-bold text-xl uppercase">Hublish</span>
-      <div class="ml-auto flex items-center gap-x-4">
+      <RouterLink to="/">
+        <span class="font-bold text-xl uppercase">Hublish</span>
+      </RouterLink>
+      <div v-if="!user.isLoggedIn" class="ml-auto flex items-center gap-x-4">
         <RouterLink class="hover:text-green-500" to="/login"> Log in </RouterLink>
         <RouterLink class="hover:text-green-500" to="/signup"> Sign up </RouterLink>
+      </div>
+
+      <div
+        @click.stop="isMenuOpened = !isMenuOpened"
+        v-else
+        class="ml-auto flex items-center gap-x-4 relative group cursor-pointer"
+      >
+        <div class="h-10 w-10 rounded-full bg-gray-300" />
+        <div class="sm:block hidden">
+          <span v-if="user.user?.name">{{ user.user?.name }}</span>
+          <span v-else>{{ user.user?.username }}</span>
+        </div>
+        <div>
+          <IconChevronDown class="h-6 w-6"></IconChevronDown>
+        </div>
+        <div
+          v-show="isMenuOpened"
+          ref="menu"
+          class="w-full min-w-[200px] bg-white absolute top-[140%] right-0 p-2"
+        >
+          <RouterLink class="px-4 py-2 block text-center hover:bg-gray-200" :to="`/settings`">
+            Settings
+          </RouterLink>
+          <RouterLink class="px-4 py-2 block text-center hover:bg-gray-200" to="/">
+            Log out
+          </RouterLink>
+        </div>
       </div>
     </div>
   </header>
