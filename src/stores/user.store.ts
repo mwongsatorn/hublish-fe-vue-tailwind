617b66909d2bc578e678604013da61e5dc0d67fa
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-10-27 15:41:31 +0700

    fix: remove response validation in user store

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,7 +1,6 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
-import { z } from 'zod'
-import { UserSchema, type User } from '@/schemas/user'
+import { type User } from '@/schemas/user'
 
 interface UserState {
   user: User | null
@@ -17,41 +16,31 @@ export const useUserStore = defineStore('user', {
   }),
   actions: {
     async login(details: { username: string; password: string }) {
-      const response = await axios.post('/api/auth/login', details)
+      const response = await axios.post<User>('/api/auth/login', details)
 
       if (response.status === 401) throw 'Username or password is incorrect'
-
-      const validateRes = UserSchema.safeParse(response.data)
-
-      if (!validateRes.success) throw 'Something went wrong. Please try again later.'
-      const { data } = validateRes
-      this.user = data
+      this.user = response.data
     },
 
     async getUserProfile() {
-      const response = await axios.get('/api/users/current')
+      const response = await axios.get<User>('/api/users/current')
       if (response.status === 401) {
         await this.refreshAccessToken()
       }
-      const validateRes = UserSchema.safeParse(response.data)
-      if (!validateRes.success) throw 'Something went wrong'
-      const { data } = validateRes
-      this.user = data
+      this.user = response.data
     },
 
     async refreshAccessToken() {
-      const response = await axios.get('/api/auth/refresh')
+      const response = await axios.get<{ accessToken: string }>('/api/auth/refresh')
       if (response.status !== 200) return
-      const result = z.object({ accessToken: z.string() }).safeParse(response.data)
-      if (!result.success) throw 'Something went wrong'
-      this.accessToken = result.data.accessToken
+      this.accessToken = response.data.accessToken
       await this.getUserProfile()
       this.isLoggedIn = true
     },
 
     async logOut() {
       const response = await axios.delete('/api/auth/logout')
-      if (response.status !== 200) throw 'Something went wrong'
+      if (response.status !== 204) throw 'Something went wrong'
       this.isLoggedIn = false
       this.user = null
     }
