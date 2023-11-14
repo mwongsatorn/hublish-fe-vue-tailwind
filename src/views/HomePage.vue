071dda5e221ffa395fa4f0d6e1f07aff51528574
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-11-14 16:25:04 +0700

    feat: display latest article on homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,32 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { type Article } from '@/schemas/article'
+import ArticleCard from '@/components/ArticleCard.vue'
+
+const latestArticles = ref<Article[] | null>(null)
+
+const response = await axios.get(`/api/articles`)
+
+latestArticles.value = response.data
+</script>
 
 <template>
-  <main class="bg-green-500">
-    <section class="max-w-7xl mx-auto px-4 py-36 text-white space-y-4">
-      <div class="font-bold text-5xl uppercase">Lorem ipsum dolor sit amet consectetur</div>
-      <div class="w-1/2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum totam minima quod maiores
-        eos saepe adipisci molestiae tenetur maxime nulla!
+  <main>
+    <section class="px-4 py-36 text-white space-y-4 bg-green-400">
+      <div class="mx-auto max-w-7xl">
+        <div class="font-bold text-5xl uppercase px-4">Lorem ipsum dolor sit amet consectetur</div>
+        <div class="w-1/2 px-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum totam minima quod maiores
+          eos saepe adipisci molestiae tenetur maxime nulla!
+        </div>
+      </div>
+    </section>
+    <section class="max-w-7xl mx-auto py-12 space-y-4">
+      <h1 class="px-4 sm:text-4xl text-2xl font-bold">Latest articles</h1>
+      <div class="mt-8">
+        <ArticleCard :article="article" v-for="article in latestArticles" :key="article.id">
+        </ArticleCard>
       </div>
     </section>
   </main>
