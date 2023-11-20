384f65340d45523ce45f0c36c6e5c2c892e160ea
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-11-20 22:29:29 +0700

    feat: TransitionFade component

---
+++ src/components/TransitionFade.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Transition
+    mode="out-in"
+    enter-active-class="transition duration-300"
+    leave-active-class="transition duration-300"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-1"
+    leave-from-class="opacity-1"
+    leave-to-class="opacity-0"
+  >
+    <slot></slot>
+  </Transition>
+</template>
