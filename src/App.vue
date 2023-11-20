a41606656e502042918327ec7ca1ab00daa7b99c
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-11-20 22:40:51 +0700

    feat: route transition at the app's root

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
+import TransitionFade from './components/TransitionFade.vue'
 </script>
 
 <template>
   <AppHeader></AppHeader>
-  <Suspense>
-    <RouterView></RouterView>
-  </Suspense>
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <TransitionFade>
+        <Suspense>
+          <component :is="Component"></component>
+        </Suspense>
+      </TransitionFade>
+    </template>
+  </RouterView>
 </template>
 
 <style></style>
