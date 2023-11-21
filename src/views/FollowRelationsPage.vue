28f7feb2b1653cf4243991d9f9e46b8d1a2569f7
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-11-21 21:46:13 +0700

    feat: route transition/keepalive on flw-rel page

--- src/views/FollowRelationsPage.vue
+++ src/views/FollowRelationsPage.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import AppLink from '@/components/AppLink.vue'
+import TransitionFade from '@/components/TransitionFade.vue'
+
+const props = defineProps<{ username: string }>()
 </script>
 
 <template>
@@ -10,7 +13,7 @@ import AppLink from '@/components/AppLink.vue'
           active-class="bg-green-500 text-white font-bold"
           inactive-class="hover:bg-gray-200"
           class="p-4 grow text-center bg-gray-100"
-          :to="{ name: 'UserFollowers' }"
+          :to="{ name: 'UserFollowers', params: { username: props.username } }"
         >
           Followers
         </AppLink>
@@ -18,12 +21,24 @@ import AppLink from '@/components/AppLink.vue'
           active-class="bg-green-500 text-white font-bold"
           inactive-class="hover:bg-gray-200"
           class="p-4 grow text-center bg-gray-100"
-          :to="{ name: 'UserFollowings' }"
+          :to="{ name: 'UserFollowings', params: { username: props.username } }"
         >
           Followings
         </AppLink>
       </div>
     </section>
-    <RouterView> </RouterView>
+    <section class="py-4">
+      <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+          <TransitionFade>
+            <KeepAlive>
+              <Suspense>
+                <component :is="Component"></component>
+              </Suspense>
+            </KeepAlive>
+          </TransitionFade>
+        </template>
+      </RouterView>
+    </section>
   </main>
 </template>
