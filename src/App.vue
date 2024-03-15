b01edd8b2ce41ebe485122e14fb9180d262496c8
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-03-15 19:42:34 +0700

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 import TransitionFade from './components/TransitionFade.vue'
 </script>
 
 <template>
   <AppHeader></AppHeader>
-  <RouterView v-slot="{ Component, route }">
-    <template v-if="Component">
-      <TransitionFade>
-        <Suspense>
-          <component :is="Component" :key="route.params.username"></component>
-        </Suspense>
-      </TransitionFade>
-    </template>
-  </RouterView>
+  <main class="min-h-[calc(100vh-56px)]">
+    <RouterView v-slot="{ Component, route }">
+      <template v-if="Component">
+        <TransitionFade>
+          <Suspense>
+            <component :is="Component" :key="route.params.username"></component>
+          </Suspense>
+        </TransitionFade>
+      </template>
+    </RouterView>
+  </main>
+  <AppFooter></AppFooter>
 </template>
 
 <style></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script setup type="ts">
+import IconGithub from '@/components/icons/Github.vue'
+</script>
+
+<template>
+  <footer class="relative w-full border-t-2 mt-auto bg-gray-700">
+    <div class="mx-auto max-w-7xl px-4 py-4 flex items-center justify-center">
+      <a
+        class="flex gap-x-4 font-bold text-xl text-white hover:underline-offset-2 hover:underline"
+        href="https://github.com/mwongsatorn/hublish"
+      >
+        View all codes on Github
+        <IconGithub class="size-8 text-white"></IconGithub>
+      </a>
+    </div>
+  </footer>
+</template>
