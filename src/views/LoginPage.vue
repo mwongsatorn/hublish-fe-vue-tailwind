fbbff1ad6cf86c54cd9c578d551dd7929775d8f9
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-03-17 21:35:17 +0700

    fix(style): structure on login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -26,8 +26,8 @@ async function loginSubmit() {
     await user.login(validate.data)
 
     router.push('/')
-  } catch (e) {
-    loginError.value = e as string
+  } catch (e: any) {
+    loginError.value = e.response.data.error
   }
 }
 
@@ -37,47 +37,43 @@ watch(loginForm.value, () => {
 </script>
 
 <template>
-  <main>
-    <section
-      class="max-w-7xl mx-auto w-full px-4 py-12 min-h-[calc(100vh-56px)] flex items-center justify-center"
-    >
-      <div class="max-w-md w-full py-12 px-6 shadow-lg border">
-        <h1 class="font-bold text-2xl text-center">Log in</h1>
-        <p class="mt-4 text-red-700 text-center" v-if="loginError">
-          {{ loginError }}
-        </p>
-        <form class="" @submit.prevent="loginSubmit()">
-          <div class="mt-4">
-            <label for="username">Username</label>
-            <input
-              v-model="loginForm.username"
-              id="username"
-              class="block w-full mt-2 border-2 px-4 py-1.5"
-              placeholder="Username"
-              type="text"
-            />
-            <p class="text-red-700" v-if="formError?.username">
-              ** {{ formError.username._errors[0] }} **
-            </p>
-          </div>
-          <div class="mt-4">
-            <label for="password">Password</label>
-            <input
-              v-model="loginForm.password"
-              id="password"
-              class="block w-full mt-2 border-2 px-4 py-2"
-              placeholder="Password"
-              type="password"
-            />
-            <p class="text-red-700" v-if="formError?.password">
-              ** {{ formError.password._errors[0] }} **
-            </p>
-          </div>
-          <button class="ml-auto rounded-lg mt-8 block bg-green-500 px-4 py-2 text-white">
-            Log in
-          </button>
-        </form>
-      </div>
-    </section>
-  </main>
+  <div
+    class="max-w-7xl mx-auto w-full py-12 min-h-[calc(100vh-56px)] flex items-center justify-center"
+  >
+    <div class="max-w-md w-full py-12 px-4 shadow-lg border bg-white space-y-4">
+      <h1 class="font-bold text-3xl text-center">Log in</h1>
+      <p class="text-red-700 text-center" v-if="loginError">** {{ loginError }} **</p>
+      <form class="space-y-4" @submit.prevent="loginSubmit()">
+        <div class="space-y-2">
+          <label for="username">Username</label>
+          <input
+            v-model="loginForm.username"
+            id="username"
+            class="block w-full border-2 px-4 py-1.5"
+            placeholder="Username"
+            type="text"
+          />
+          <p class="text-red-700" v-if="formError?.username">
+            ** {{ formError.username._errors[0] }} **
+          </p>
+        </div>
+        <div class="space-y-2">
+          <label for="password">Password</label>
+          <input
+            v-model="loginForm.password"
+            id="password"
+            class="block w-full border-2 px-4 py-2"
+            placeholder="Password"
+            type="password"
+          />
+          <p class="text-red-700" v-if="formError?.password">
+            ** {{ formError.password._errors[0] }} **
+          </p>
+        </div>
+        <button class="ml-auto rounded-lg mt-8 block bg-green-500 px-4 py-2 text-white">
+          Log in
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
